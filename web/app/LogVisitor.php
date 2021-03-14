e7cafd5f6cc1f733b280f846d451466731030678
nepnep <ealihanip@gmail.com>
2021-03-14 14:36:53 +0700

    update log visitor

--- web/app/LogVisitor.php
+++ web/app/LogVisitor.php
@@ -8,7 +8,8 @@ class LogVisitor extends Model
 {
     protected $fillable = [
         'ip',
-        'host'
+        'host',
+        'count'
     ];
 
     public static function createData($request)
@@ -16,5 +17,18 @@ class LogVisitor extends Model
         $data = new self;
         $data->fill($request);
         $data->save();
+    }
+
+    public static function findByIp($ip)
+    {
+        return self::where('ip', $ip)->first();
+    }
+
+    public function addCount()
+    {
+        $this->count = $this->count + 1;
+        $this->save();
+
+        return $this;
     }
 }
